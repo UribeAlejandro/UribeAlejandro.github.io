a00f76a50f8358a1cf83e4e5fd3b51c2763296da
Alejandro Uribe <jauriberamirez@gmail.com>
2021-12-07 07:49:47 -0500

    First try to solve issue with landing page

--- pages.js
+++ pages.js
@@ -1,7 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/UribeAlejandro/UribeAlejandro.github.io.git";
-
+const repoURL = "https://github.com/UribeAlejandro/UribeAlejandro.github.io";
 ghpages.publish(
   pathname,
   {
