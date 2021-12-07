62bdead1a667bd189364efd4f5d076f2dbeb5053
Alejandro Uribe <jauriberamirez@gmail.com>
2021-12-07 07:43:43 -0500

    First Approach

---
+++ src/components/home/SkillsBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
+
+//function SkillsBar({ skill, value, isScrolled }) {
+function SkillsBar({ skill, isScrolled }) {
+  return (
+    <div style={{ width: "95%" }}>
+      <p className="lead mb-1 mt-2">{skill}</p>
+    </div>
+  );
+}
+
+export default SkillsBar;
